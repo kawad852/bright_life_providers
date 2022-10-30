2d2a3db756ec04527e54ca22da67f76a73049b00
khaled awad <kld.awad1996@gmail.com>
2022-10-30 11:12:50 +0200

    new commit

--- lib/models/orders_model.dart
+++ lib/models/orders_model.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class OrderModel {
   final String status;
+  final String type;
   final int orderId;
   final Timestamp createdAt;
 
@@ -9,17 +10,20 @@ class OrderModel {
     required this.orderId,
     required this.createdAt,
     required this.status,
+    required this.type,
   });
 
   static OrderModel fromJson(Map<String, dynamic> json) => OrderModel(
         orderId: json['order_id'],
         createdAt: json['created_at'],
         status: json['status'],
+        type: json['type'],
       );
 
   Map<String, dynamic> toJson() => {
         'order_id': orderId,
         'created_at': createdAt,
         'status': status,
+        'type': type,
       };
 }
