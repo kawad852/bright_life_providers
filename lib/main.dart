d7b0d1ff8b4b2fb37d0fab203903494d911f7b09
khaled <kawad852@gmail.com>
2022-11-04 15:12:01 +0200

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 //khaled basem awad .. my saved project // ...
-// ddd
+// dddtt
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
